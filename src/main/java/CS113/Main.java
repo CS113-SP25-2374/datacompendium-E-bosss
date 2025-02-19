11f183b052a102da11fd7cedbc3d89a9a6d306dd
Ethan Senger <esenger02@icloud.com>
2025-02-18 20:29:47 -0800

    arraylist

--- src/main/java/CS113/Main.java
+++ src/main/java/CS113/Main.java
@@ -3,5 +3,13 @@ package CS113;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        ArrayListES<Integer> list = new ArrayListES<>();
+
+        for(int i = 0; i < 10; i++){
+            list.add(i);
+        }
+
+        System.out.println(list);
     }
 }
